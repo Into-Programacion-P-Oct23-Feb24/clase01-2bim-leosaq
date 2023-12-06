30096e8c4bf17de12245bc128af72dd0299a042d
leosaq <leandrosaquisari7@gmail.com>
2023-12-06 15:43:12 -0500

    Ejemplos

--- ManejoCicloFor/src/paquete01/Ejemplo04.java
+++ ManejoCicloFor/src/paquete01/Ejemplo04.java
@@ -15,7 +15,7 @@ public class Ejemplo04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        // Este codigo va restando de 2 en 2 desde la cantidad inicial declarada
         for (int i = 20; i >= 0; i-=2) {
             System.out.printf("%d\n", i);
         }
